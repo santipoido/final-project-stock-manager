2a43cee3b7dbfce25d17fda514042969d295ed68
Santiago Poidomani <santipoido2005@gmail.com>
2025-05-12 09:52:15 -0300

    Init commit

---
+++ src/main/java/com/tpfinal/stockmanager/model/implementations/Product.java
@@ -0,0 +1,30 @@
+package com.tpfinal.stockmanager.model.implementations;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @PositiveOrZero
+    private int stock;
+
+    @PositiveOrZero
+    private float price;
+    // category
+}
---
+++ src/main/java/com/tpfinal/stockmanager/model/implementations/User.java
@@ -0,0 +1,23 @@
+package com.tpfinal.stockmanager.model.implementations;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String passw;
+    //rol
+}
