560e011b066a500f8bf8f8efae8308aa24910f05
Santiago Poidomani <santipoido2005@gmail.com>
2025-06-17 08:26:00 -0300

    Change user session time

--- src/main/java/com/tpfinal/stockmanager/security/JwtService.java
+++ src/main/java/com/tpfinal/stockmanager/security/JwtService.java
@@ -21,7 +21,7 @@ public class JwtService {
                 .setSubject(userDetails.getUsername())
                 .claim("role", userDetails.getAuthorities().iterator().next().getAuthority())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 1))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
